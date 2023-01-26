95006897f8847060ac88c9ea35e4fa49d3c59888
Juho Keto-Tokoi <juho.ktokoi@outlook.com>
2023-01-26 13:14:38 +0200

    Added example component structure

---
+++ src/api/mock/geocaches.ts
@@ -0,0 +1,15 @@
+
+const geoCacheMockData = [
+    {
+        latitude: 12,
+        longitude: 13
+    },
+    {
+        latitude: 12,
+        longitude: 13
+    },
+];
+
+export const getGeoCaches = () => {
+    return geoCacheMockData;
+};
---
+++ src/pages/Homepage/Homepage.tsx
@@ -0,0 +1,9 @@
+
+const Homepage = () => {
+    return (
+        <>
+        </>
+    );
+};
+
+export default Homepage;
