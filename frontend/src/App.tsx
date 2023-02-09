27642ce64add881ad40ae129d1729fe9ff3ff08d
Juho Keto-Tokoi <juho.ktokoi@outlook.com>
2023-02-09 14:07:25 +0200

    Added routing for HomePage and MapViewPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
 import HomePage from "./pages/HomePage";
+import { Route, Routes } from "react-router";
+import MapViewPage from "./pages/MapViewPage";
 
 function App() {
     return (
         <div className="App">
-            <HomePage />
+            <Routes>
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="/map" element={<MapViewPage/>}/>
+            </Routes>
         </div>
     );
 }
