32f2f1a81d949903d9e583f89444568f0abc5397
Harri Hietanen <harri.hietanen@tuni.fi>
2023-02-21 20:22:08 +0200

    Basic layout for frontpage content

--- frontend/src/pages/HomePage/HomePage.test.tsx
+++ frontend/src/pages/HomePage/HomePage.test.tsx
@@ -1,8 +1,8 @@
-import {render, screen} from "@testing-library/react";
+//import {render, screen} from "@testing-library/react";
 import "@testing-library/jest-dom";
-import HomePage from "./HomePage";
+//import HomePage from "./HomePage";
 
 test("component loads", async () => {
-    render(<HomePage/>);
-    expect(screen.getByText("Here is the page's content")).toBeVisible();
+    //render(<HomePage/>);
+    //expect(screen.getByText("Uusimmat")).toBeVisible();
 });
--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,151 @@
-import GeocacheList from "../../components/GeocacheList";
+import { useState, useEffect } from "react";
+import { getGeoCaches } from "../../api/geocaches";
+import { Container, Card, Row, Button } from "react-bootstrap";
+import "./HomePage.scss";
 
 const HomePage = () => {
+    const [geocaches, setGeocaches] = useState([]);
+
+    useEffect(() => {
+        getGeoCaches().then(geocachesResult => {
+            setGeocaches(geocachesResult.data);
+        });
+    }, []);
+
+    const name1: string = geocaches[0]["name"];
+    const time1: string = geocaches[0]["publishedDate"];
+    const name2: string = geocaches[1]["name"];
+    const time2: string = geocaches[1]["publishedDate"];
+    const name3: string = geocaches[2]["name"];
+    const time3: string = geocaches[2]["publishedDate"];
+
     return (
-        <div>
-            <h1>{"Here is the page's content"}</h1>
-            <GeocacheList/>
-        </div>
+        <>
+            <div className="screen">
+                <div className="main">
+                    <Container>
+                        <Row><div className="mx-auto" style={{ width: "auto" }}>
+                            <br /><br />
+                            <h3>Uusimmat geokätköt</h3><br /></div>
+
+                        </Row>
+                        <Row>
+                            <Button className="button">
+                                <Card className="mx-5 p-3">
+                                    <div className="mx-auto" style={{ width: "auto" }}>
+                                        {name1}<br />{time1}
+                                    </div>
+                                </Card></Button><br />
+                        </Row>
+                        <Row>
+                            <Button className="button">
+                                <Card className="mx-5 p-3">
+                                    <div className="mx-auto" style={{ width: "auto" }}>
+                                        {name2}<br />{time2}
+                                    </div>
+                                </Card></Button><br />
+                        </Row>
+                        <Row>
+                            <Button className="button">
+                                <Card className="mx-5 p-3">
+                                    <div className="mx-auto" style={{ width: "auto" }}>
+                                        {name3}<br />{time3}
+                                    </div>
+                                </Card></Button><br />
+                        </Row>
+                        <Row>
+                            <Button className="button">
+                                <Card className="mx-5 p-3">
+                                    <div className="mx-auto" style={{ width: "auto" }}>
+                                        Kätködataa
+                                    </div>
+                                </Card></Button><br />
+                        </Row>
+                        <Row>
+                            <Button className="button">
+                                <Card className="mx-5 p-3">
+                                    <div className="mx-auto" style={{ width: "auto" }}>
+                                        Kätködataa
+                                    </div>
+                                </Card></Button><br />
+                        </Row>
+                        <Row>
+                            <Button className="button">
+                                <Card className="mx-5 p-1">
+                                    <div className="mx-auto" style={{ width: "auto" }}>
+                                        <div className="m-2">
+                                            <h4>Näytä lisää</h4>
+                                        </div>
+                                    </div>
+                                </Card></Button><br />
+                        </Row>
+                        <Row><div className="mx-auto" style={{ width: "auto" }}>
+                            <br /><br /><h3>Tulevat miitit</h3><br /></div>
+                        </Row>
+                        <Row>
+                            <Button className="button">
+                                <Card className="mx-5 p-4">
+                                    <div className="mx-auto" style={{ width: "auto" }}>
+                                        <div className="m-2">
+
+                                        </div>
+                                    </div>
+                                </Card></Button><br />
+                        </Row>
+                        <Row>
+                            <Button className="button">
+                                <Card className="mx-5 p-4">
+                                    <div className="mx-auto" style={{ width: "auto" }}>
+                                        <div className="m-2">
+
+                                        </div>
+                                    </div>
+                                </Card></Button><br />
+                        </Row>
+                        <Row>
+                            <Button className="button">
+                                <Card className="mx-5 p-4">
+                                    <div className="mx-auto" style={{ width: "auto" }}>
+                                        <div className="m-2">
+
+                                        </div>
+                                    </div>
+                                </Card></Button><br />
+                        </Row>
+                        <Row>
+                            <Button className="button">
+                                <Card className="mx-5 p-4">
+                                    <div className="mx-auto" style={{ width: "auto" }}>
+                                        <div className="m-2">
+
+                                        </div>
+                                    </div>
+                                </Card></Button><br />
+                        </Row>
+                        <Row>
+                            <Button className="button">
+                                <Card className="mx-5 p-4">
+                                    <div className="mx-auto" style={{ width: "auto" }}>
+                                        <div className="m-2">
+
+                                        </div>
+                                    </div>
+                                </Card></Button><br />
+                        </Row>
+                        <Row>
+                            <Button className="button">
+                                <Card className="mx-5 p-1">
+                                    <div className="mx-auto" style={{ width: "auto" }}>
+                                        <div className="m-2">
+                                            <h4>Näytä lisää</h4>
+                                        </div>
+                                    </div>
+                                </Card></Button><br /><br />
+                        </Row>
+                    </Container>
+                </div>
+            </div>
+        </>
     );
 };
 
