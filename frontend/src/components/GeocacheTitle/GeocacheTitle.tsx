9e5e8a0c61231e43019e20b177b129d53e3c3742
Juho Keto-Tokoi <juho.ktokoi@outlook.com>
2023-04-23 23:46:29 +0300

    Added logic for loading more geocaches and a spinner that indicates when the loading is in progress

--- frontend/src/components/GeocacheTitle/GeocacheTitle.tsx
+++ frontend/src/components/GeocacheTitle/GeocacheTitle.tsx
@@ -1,47 +1,58 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import "./GeocacheTitle.scss";
 
 interface Props {
     date: Date;
-  }
+}
 
-const GeocacheTitle = (props: Props) => {
+const GeocacheTitle = ({ date }: Props) => {
     const [isNew, setIsNew] = useState(false);
     const [minutesAgo, setMinutesAgo] = useState(0);
-    const checkIsNew = () => {
+
+    const checkIsNew = useCallback(() => {
         const HOUR = 1000 * 60 * 60;
         const anHourAgo = Date.now() - HOUR;
-        if (props.date.getTime() > anHourAgo) {
+        if (date.getTime() > anHourAgo) {
             setIsNew(true);
         }
         else {
             setIsNew(false);
-        } 
-    };
-    const getMinutesAgo = () => {
-        const millis = Date.now() - props.date.getTime();
+        }
+    }, [date]);
+
+    const getMinutesAgo = useCallback(() => {
+        const millis = Date.now() - date.getTime();
         setMinutesAgo(Math.floor(millis / 60000));
+    },[date]);
+
+    const formatTime = (num: number) => {
+        return num < 10 ? `0${num}` : num.toString();
     };
-    const formatDate = () => {
-        return props.date.getDay()+"."+props.date.getMonth()+"."+props.date.getFullYear()+" "+props.date.getHours()+":"+props.date.getMinutes();
+
+    const formatDateTime = () => {
+        return `${formatTime(date.getDate())}.${formatTime(date.getMonth() + 1)}.${date.getFullYear()} ${formatTime(date.getHours())}:${formatTime(date.getMinutes())}`;
     };
+
     const setContent = () => {
-        if(isNew){
-            return <p className="new-cache-title">{minutesAgo} minuuttia</p>;
+        if (isNew) {
+            return <p className="new-cache-title">{minutesAgo} minuuttia sitten</p>;
         }
-        else{
-            return <p>{formatDate()}</p>;
+        else {
+            return <p>{formatDateTime()}</p>;
         }
     };
-    const update = () => {
+
+    const update = useCallback(() => {
         checkIsNew();
         getMinutesAgo();
-    };
+    }, [checkIsNew, getMinutesAgo]);
+
     useEffect(() => {
         update();
         const interval = setInterval(update, 60000);
         return () => clearInterval(interval);
-    });
+    }, [update]);
+
     return (
         <div data-testid="geocache-title-div">
             {setContent()}
