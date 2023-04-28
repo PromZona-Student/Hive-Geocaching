c05c2c7d5c333a3bb8f29807b8da48853f27403d
Mel <melmendozaromero@gmail.com>
2023-04-28 23:40:18 +0300

    70 attributes filter implementation 1/3 bug fixes

--- frontend/src/components/MapFilters/AttributesFilter/AttributesFilter.tsx
+++ frontend/src/components/MapFilters/AttributesFilter/AttributesFilter.tsx
@@ -1,5 +1,5 @@
 import "./AttributesFilter.scss";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useState, useEffect } from "react";
 import { Attributes } from "../../../model/Filters";
 import MapFilterItem from "../../MapFilterItem";
 import { abandonedBuildingNo,abandonedBuildingYes,availableNo,availableYes,bicyclesNo,bicyclesYes, boatYes,challengecacheYes,climbingNo,climbingYes,fieldPuzzleNo, fieldPuzzleYes,flashlightYes,hikeShortNo,hikeShortYes,mineYes,nightNo,nightYes,parkingNo,parkingYes,parkngrabNo,parkngrabYes,powertrailYes,rappellingYes,snowshoesYes,sToolYes,teamworkNo,teamworkYes,treeclimbingNo,treeclimbingYes,uVYes,wadingYes,winterNo,winterYes } from "../../../images/attributes";
@@ -14,132 +14,128 @@ interface AttributeDisplay{
     name: keyof Attributes;
     image: string;
     title: string;
-    oppositeName: string;
     isDisabled: boolean;
     style: string;
+    opposite: string;
+    oppositeKey?: keyof Attributes;
 }
 
-const disabledStatesArray = [
-    { name: "abandonedBuildingNo", isDisabled: false, style: "check-box-filter" },
-    { name: "abandonedBuildingYes", isDisabled: false, style: "check-box-filter" },
-    { name: "availableNo", isDisabled: false, style: "check-box-filter" },
-    { name: "availableYes", isDisabled: false, style: "check-box-filter" },
-    { name: "bicyclesNo", isDisabled: false, style: "check-box-filter" },
-    { name: "bicyclesYes", isDisabled: false, style: "check-box-filter" },
-    { name: "boatYes", isDisabled: false, style: "check-box-filter" },
-    { name: "challengecacheYes", isDisabled: false, style: "check-box-filter" },
-    { name: "climbingNo", isDisabled: false, style: "check-box-filter" },
-    { name: "climbingYes", isDisabled: false, style: "check-box-filter" },
-
-    { name: "fieldPuzzleNo", isDisabled: false, style: "check-box-filter" },
-    { name: "fieldPuzzleYes", isDisabled: false, style: "check-box-filter" },
-    { name: "flashlightYes", isDisabled: false, style: "check-box-filter" },
-    { name: "hikeShortNo", isDisabled: false, style: "check-box-filter" },
-    { name: "hikeShortYes", isDisabled: false, style: "check-box-filter" },
-    { name: "mineYes", isDisabled: false, style: "check-box-filter" },
-    { name: "nightNo", isDisabled: false, style: "check-box-filter" },
-    { name: "nightYes", isDisabled: false, style: "check-box-filter" },
-    { name: "parkingNo", isDisabled: false, style: "check-box-filter" },
-    { name: "parkingYes", isDisabled: false, style: "check-box-filter" },
-
-    { name: "parkngrabNo", isDisabled: false, style: "check-box-filter" },
-    { name: "parkngrabYes", isDisabled: false, style: "check-box-filter" },
-    { name: "powertrailYes", isDisabled: false, style: "check-box-filter" },
-    { name: "rappellingYes", isDisabled: false, style: "check-box-filter" },
-    { name: "snowshoesYes", isDisabled: false, style: "check-box-filter" },
-    { name: "sToolYes", isDisabled: false, style: "check-box-filter" },
-    { name: "teamworkNo", isDisabled: false, style: "check-box-filter" },
-    { name: "teamworkYes", isDisabled: false, style: "check-box-filter" },
-    { name: "treeclimbingNo", isDisabled: false, style: "check-box-filter" },
-    { name: "treeclimbingYes", isDisabled: false, style: "check-box-filter" },
-
-    { name: "uVYes", isDisabled: false, style: "check-box-filter" },
-    { name: "wadingYes", isDisabled: false, style: "check-box-filter" },
-    { name: "winterNo", isDisabled: false, style: "check-box-filter" },
-    { name: "winterYes", isDisabled: false, style: "check-box-filter" },
-];
-
 const DEFAULT_DISPLAY_VALUE = false;
+
+const getIsDisabled = (oppositeKey:keyof Attributes, attributes?:Attributes) => {
+    if(oppositeKey){
+        const isChecked = attributes?.[oppositeKey];
+        if(isChecked){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    else{
+        return false;
+    }
+};
+
+const initAttrArray = (attributes?:Attributes) => {
+    console.log("INIT");
+    const keys: (keyof Attributes)[] = ["abandonedBuildingNo","abandonedBuildingYes","availableNo","availableYes","bicyclesNo","bicyclesYes","boatYes","challengecacheYes","climbingNo","climbingYes","fieldPuzzleNo","fieldPuzzleYes", "flashlightYes","hikeShortNo","hikeShortYes","mineYes","nightNo","nightYes","parkingNo","parkingYes","parkngrabNo","parkngrabYes","powertrailYes","rappellingYes","snowshoesYes","sToolYes","teamworkNo","teamworkYes","treeclimbingNo","treeclimbingYes","uVYes","wadingYes","winterNo","winterYes"];
+    const images = [abandonedBuildingNo,abandonedBuildingYes,availableNo,availableYes,bicyclesNo,bicyclesYes, boatYes,challengecacheYes,climbingNo,climbingYes,fieldPuzzleNo, fieldPuzzleYes, flashlightYes,hikeShortNo,hikeShortYes,mineYes,nightNo,nightYes,parkingNo,parkingYes,parkngrabNo,parkngrabYes,powertrailYes,rappellingYes,snowshoesYes,sToolYes,teamworkNo,teamworkYes,treeclimbingNo,treeclimbingYes,uVYes,wadingYes,winterNo,winterYes];
+    const titles = ["Ei raunioita","Raunioita","Ei saatavilla ympäri vuorokauden","Käytettävissä ympäri vuorokauden","Ei sovellu polkupyörille","Soveltuu polkupyörille","Tarvitsee veneen","Haastekätkö","Ei vaikeaa nousua","Vaikea nousu","Ei field puzzlea","Field puzzle","Tarvitsee taskulampun","Vähintään 1 km:n matka","Lyhyt,alle 1 km:n matka","Varo hylättyjä kaivoksia","Yökäyntiä ei suositella","Käyntiä suositellaan yöllä","Ei parkkipaikkaa","Pysäköintialue","Ei drive-in -kätkö","Drive-in -kätkö","Power trail","Tarvitsee kiipeilyvälineet","Voi tarvita lumikengät","Tarvitsee erikoistyökalun","Ei ryhmätyökätköä","Tiimityökätkö","Ei puukiipeilykätköä","Puuhunkiipeämiskätkö","Tarvitsee UV-lampun","Voi vaatia kahlaamista","Ei löydy lumisena aikana","Löytyy myös lumisena aikana"];
+    const opposites = ["abandonedBuildingYes","abandonedBuildingNo","availableYes","availableNo","bicyclesYes","bicyclesNo","","","climbingYes","climbingNo","fieldPuzzleYes","fieldPuzzleNo","","hikeShortYes","hikeShortNo","","nightYes","nightNo","parkingYes","parkingNo","parkngrabYes","parkngrabNo","","","","","teamworkYes","teamworkNo","treeclimbingYes","treeclimbingNo","","","winterYes","winterNo"];
+    const oppositeKeys : (keyof Attributes)[] =  ["abandonedBuildingYes","abandonedBuildingNo","availableYes","availableNo","bicyclesYes","bicyclesNo","abandonedBuildingYes","abandonedBuildingYes","climbingYes","climbingNo","fieldPuzzleYes","fieldPuzzleNo","abandonedBuildingYes","hikeShortYes","hikeShortNo","abandonedBuildingYes","nightYes","nightNo","parkingYes","parkingNo","parkngrabYes","parkngrabNo","abandonedBuildingYes","abandonedBuildingYes","abandonedBuildingYes","abandonedBuildingYes","teamworkYes","teamworkNo","treeclimbingYes","treeclimbingNo","abandonedBuildingYes","abandonedBuildingYes","winterYes","winterNo"];
+    
+    const attrArray: Array<AttributeDisplay> = [];
+    for (let i = 0; i < keys.length; i++) {
+        let attr;
+        if(opposites[i]!=""){
+            const isDisabled = getIsDisabled(oppositeKeys[i], attributes);
+            if(isDisabled){
+                attr = {
+                    name: keys[i],
+                    image: images[i],
+                    title: titles[i],
+                    isDisabled: isDisabled,
+                    style: "check-box-filter disabled",
+                    opposite: opposites[i],
+                    oppositeKey: oppositeKeys[i]
+    
+                }; 
+            } else{
+                attr = {
+                    name: keys[i],
+                    image: images[i],
+                    title: titles[i],
+                    isDisabled: isDisabled,
+                    style: "check-box-filter",
+                    opposite: opposites[i],
+                    oppositeKey: oppositeKeys[i]
+    
+                }; 
+            }
+        }
+        else{
+            attr = {
+                name: keys[i],
+                image: images[i],
+                title: titles[i],
+                isDisabled: false,
+                style: "check-box-filter",
+                opposite: opposites[i]
+            };  
+        }
+        attrArray.push(attr);
+    }
+    return attrArray;
+};
 
 const AttributesFilter = ({
     onChange,
     attributes,
     eventKey
 }: Props) => {
-    const [disabledStates, setDisabledStates] = useState(disabledStatesArray);
-
+    const [attrArray, setAttrArray] = useState(initAttrArray(attributes));
+ 
     const modifyAttributes = (e: ChangeEvent<HTMLInputElement>) => {
         const name = e.target.name as keyof Attributes;
         const value = e.target.checked;
         const oppositeName = e.currentTarget.getAttribute("data-value");
-        if(value==true && oppositeName!=null && oppositeName!=""){
-            updateState(oppositeName, true, "check-box-filter disabled");
+        if(oppositeName!=null && oppositeName!=""){
+            if(value==true){
+                updateIsDisabled(oppositeName, true);
+            }
+            else{
+                updateIsDisabled(oppositeName, false);
+            }
         }
-        if(value==false && oppositeName!=null && oppositeName!=""){
-            updateState(oppositeName, false, "check-box-filter");
-        }
+        
         onChange({
             ...attributes,
             [name]: value
         });
     };
 
-    const updateState= (name:string, updatedIsDisabled:boolean, style:string) => {
-        setDisabledStates(
-            disabledStates.map((state) =>
-                state.name === name
-                    ? { ...state, isDisabled: updatedIsDisabled, style: style }
-                    : { ...state }
-            )
-        );
-    };
-
-    const findStateByName= (name:string) => {
-        const state = disabledStates.find(state => state.name === name);
-        if(state){
-            return(state.isDisabled);
-        }
-        else{
-            return(false);
-        }
-    };
-
-    const findStyleByName= (name:string) => {
-        const state = disabledStates.find(state => state.name === name);
-        if(state){
-            return(state.style);
-        }
-        else{
-            return( "check-box-filter");
-        }
-    };
-
-    const initAttrArray = () => {
-        const keys: (keyof Attributes)[] = ["abandonedBuildingNo","abandonedBuildingYes","availableNo","availableYes","bicyclesNo","bicyclesYes","boatYes","challengecacheYes","climbingNo","climbingYes","fieldPuzzleNo","fieldPuzzleYes", "flashlightYes","hikeShortNo","hikeShortYes","mineYes","nightNo","nightYes","parkingNo","parkingYes","parkngrabNo","parkngrabYes","powertrailYes","rappellingYes","snowshoesYes","sToolYes","teamworkNo","teamworkYes","treeclimbingNo","treeclimbingYes","uVYes","wadingYes","winterNo","winterYes"];
-        const names = ["abandonedBuildingNo","abandonedBuildingYes","availableNo","availableYes","bicyclesNo","bicyclesYes","boatYes","challengecacheYes","climbingNo","climbingYes","fieldPuzzleNo","fieldPuzzleYes", "flashlightYes","hikeShortNo","hikeShortYes","mineYes","nightNo","nightYes","parkingNo","parkingYes","parkngrabNo","parkngrabYes","powertrailYes","rappellingYes","snowshoesYes","sToolYes","teamworkNo","teamworkYes","treeclimbingNo","treeclimbingYes","uVYes","wadingYes","winterNo","winterYes"];
-        const images = [abandonedBuildingNo,abandonedBuildingYes,availableNo,availableYes,bicyclesNo,bicyclesYes, boatYes,challengecacheYes,climbingNo,climbingYes,fieldPuzzleNo, fieldPuzzleYes, flashlightYes,hikeShortNo,hikeShortYes,mineYes,nightNo,nightYes,parkingNo,parkingYes,parkngrabNo,parkngrabYes,powertrailYes,rappellingYes,snowshoesYes,sToolYes,teamworkNo,teamworkYes,treeclimbingNo,treeclimbingYes,uVYes,wadingYes,winterNo,winterYes];
-        const titles = ["Ei raunioita","Raunioita","Ei saatavilla ympäri vuorokauden","Käytävissä ympäri vuorokauden","Ei sovellu polkupyörille","Soveltuu polkupyörille","Tarvitsee veneen","Haastekätkö","Ei vaikeaa nousua","Vaikea nousu","Ei field puzzlea","Field puzzle","Tarvitsee taskulampun","Vähintään 1 km:n matka","Lyhyt,alle 1 km:n matka","Varo hylättyjä kaivoksia","Yökäyntiä ei suositella","Käyntiä suositellaan yöllä","Ei parkkipaikkaa","Pysäköintialue","Ei drive-in -kätkö","Drive-in -kätkö","Power trail","Tarvitsee kiipeilyvälineet","Voi tarvita lumikengät","Tarvitsee erikoistyökalun","Ei ryhmätyökätköä","Tiimityökätkö","Ei puukiipeilykätköä","Puuhunkiipeämiskätkö","Tarvitsee UV-lampun","Voi vaatia kahlaamista","Ei löydy lumisena aikana","Löytyy myös lumisena aikana"];
-        const opposites = ["abandonedBuildingYes","abandonedBuildingNo","availableYes","availableNo","bicyclesYes","bicyclesNo","","","climbingYes","climbingNo","fieldPuzzleYes","fieldPuzzleNo","","hikeShortYes","hikeShortNo","","nightYes","nightNo","parkingYes","parkingNo","parkngrabYes","parkngrabNo","","","","","teamworkYes","teamworkNo","treeclimbingYes","treeclimbingNo","","","winterYes","winterNo"];
-        const attrArray: Array<AttributeDisplay> = [];
-        for (let i = 0; i < keys.length; i++) {
-            const attr: AttributeDisplay = {
-                name: keys[i],
-                image: images[i],
-                title: titles[i],
-                oppositeName: opposites[i],
-                isDisabled: findStateByName(names[i]),
-                style:  findStyleByName(names[i])
-            };  
-            attrArray.push(attr);
-        }
-        return attrArray;
+    const updateIsDisabled= (name:string, isDisabled:boolean) => {
+        const attrArrayUpdated = attrArray;
+        attrArrayUpdated.map(attr => { 
+            if(attr.name==name){
+                attr.isDisabled=isDisabled;
+                if(isDisabled){
+                    attr.style="check-box-filter disabled";
+                }
+                else{
+                    attr.style="check-box-filter";
+                }
+            }
+        });
+        setAttrArray(attrArrayUpdated);
     };
 
     return (
         <MapFilterItem header="Attribuutit" eventKey={eventKey}>
             <div className="check-box-filters">
                 {
-                    initAttrArray().map(attr => {
+                    attrArray.map(attr => {
                         return(
                             <div className="check-box-filters" key={attr.name}>
                                 <div className={attr.style}>
@@ -148,7 +144,7 @@ const AttributesFilter = ({
                                         type="checkbox"
                                         name={attr.name}
                                         onChange={modifyAttributes}
-                                        data-value={attr.oppositeName}
+                                        data-value={attr.opposite}
                                         disabled= {attr.isDisabled}
                                         checked={attributes?.[attr.name] || DEFAULT_DISPLAY_VALUE}
                                     />
